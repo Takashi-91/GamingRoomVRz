626b08d587595f5d9b4eff9998397938c522d4f7
Takashi-91 <madimetjaterencechuene@gmail.com>
2025-04-06 15:24:59 +0200

    Sofa Fix

--- client/src/components/Couch.tsx
+++ client/src/components/Couch.tsx
@@ -4,7 +4,7 @@ import { Text } from "@react-three/drei";
 import * as THREE from "three";
 import { useInteraction } from "../lib/stores/useInteraction";
 import { useAudio } from "../lib/stores/useAudio";
-import { SofaModel } from "./sofaModel";
+import {SofaModel} from "./SofaModel";
 
 interface CouchProps {
   position?: [number, number, number];
@@ -18,26 +18,7 @@ export default function Couch({ position = [0, 0, 0], rotation = [0, 0, 0] }: Co
   const { registerInteractiveObject } = useInteraction();
   const { playHit } = useAudio();
   
-  // Register interactive elements
-  useFrame(() => {
-    if (couchRef.current && !couchRef.current.userData.registered) {
-      registerInteractiveObject(couchRef.current, "Couch & Controllers", () => {
-        console.log("Couch area interaction");
-        playHit();
-        
-        // Animated effect for the controller
-        if (controllerRef.current) {
-          const currentY = controllerRef.current.position.y;
-          // Simple animation - move controller up slightly
-          controllerRef.current.position.y = currentY + 0.05;
-          setTimeout(() => {
-            if (controllerRef.current) controllerRef.current.position.y = currentY;
-          }, 300);
-        }
-      });
-      couchRef.current.userData.registered = true;
-    }
-  });
+  // Register interactive element
 
   return (
     <group ref={couchRef} position={position} rotation={rotation} name="couch-area">
